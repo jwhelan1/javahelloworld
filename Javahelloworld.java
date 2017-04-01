69b5ca3e2a529362f3c3d9cb2a745a6c55397a7f
jwhelan1 <jimmy.whelan@gmail.com>
2017-04-01 16:48:19 -0700

    removed some hellos

--- Javahelloworld.java
+++ Javahelloworld.java
@@ -4,9 +4,6 @@ public class Javahelloworld
 	public static void main (String [] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("Hello World Again");
-		System.out.println("Hello World Again and Again");
-		System.out.println("Hello World Again and Again and Again");
 	}
 }
 
